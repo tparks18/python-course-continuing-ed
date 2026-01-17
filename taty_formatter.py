1e9a4cda7053b7de00be94a95ac1765176d8429c
Taty <tatyanaparks@gmail.com>
2026-01-17 10:02:38 -0600

    made new list

--- taty_formatter.py
+++ taty_formatter.py
@@ -1,7 +1,9 @@
 me = ['data analyst', 'reader', 'mentor']
+future_me = ['machine learning engineer', 'author', 'speaker']
 
 def formatter(lst):
     for i in lst:
         print(i)
     
 print(formatter(me))
+print(formatter(future_me))
