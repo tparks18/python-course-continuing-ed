d787d94e74212dc10f441f461f7e4b0d042be43d
Taty <tatyanaparks@gmail.com>
2026-01-17 10:08:00 -0600

    add new list again

--- taty_formatter.py
+++ taty_formatter.py
@@ -1,9 +1,12 @@
 me = ['data analyst', 'reader', 'mentor']
 future_me = ['machine learning engineer', 'author', 'speaker']
+past_me = ['student', 'intern', 'researcher']
 
 def formatter(lst):
+    #this function loops through a list and prints each item on a new line
     for i in lst:
         print(i)
     
 print(formatter(me))
 print(formatter(future_me))
+print(formatter(past_me))
